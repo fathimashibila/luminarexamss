764a6f456869f65ecc2f997518e9e7f61f99d5f8
fathimashibila <69387181+fathimashibila@users.noreply.github.com>
2020-09-18 10:51:41 +0530

    Add files via upload

---
+++ assingment.py
@@ -0,0 +1,31 @@
+f=open("complete","r")
+dict={}
+for line in f:
+    data=line.rstrip("\n").split(",")
+    state=data[1]
+    conf=data[4]
+    recv=data[5]
+    death=data[6]
+    if (state not in dict):
+        dict[state] ={"State": state, "Confrimed": conf, "Recovered": recv, "Death": death}
+    else:
+        dict[state] ={"State": state, "Confrimed": conf, "Recovered": recv, "Death": death}
+print(dict)
+def fetchData(**kwargs):
+    state=kwargs["state"]
+
+    if state not in dict:
+        print("No State")
+    else:
+        print("Confrimed Cases:",dict[state]["Confrimed"])
+def fetchData1(**kwargs):
+    prop=kwargs["prop"]
+
+    if state not in dict:
+        print("No State")
+    else:
+        print(prop,"Cases",dict[state][prop])
+
+fetchData(state="Kerala")
+fetchData1(state="kerala",prop="Recovered")
+fetchData1(state="Kerala",prop="Death")
